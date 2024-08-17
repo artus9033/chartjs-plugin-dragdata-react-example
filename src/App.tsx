23b90527a29c9e9df1eef0642da090708649c656
artus9033 <artus9033@gmail.com>
2024-08-18 00:07:13 +0200

    feat: added chartjs-plugin-dragdata

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Chart from "chart.js/auto";
+import ChartJSDragDataPlugin from "chartjs-plugin-dragdata";
+import React from "react";
+import { Line } from "react-chartjs-2";
+
+Chart.register(ChartJSDragDataPlugin);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Line
+          data={{
+            datasets: [
+              {
+                data: [
+                  {
+                    x: 1,
+                    y: 2,
+                  },
+                  {
+                    x: 2,
+                    y: 4,
+                  },
+                  {
+                    x: 3,
+                    y: 6,
+                  },
+                  {
+                    x: 4,
+                    y: 10.2,
+                  },
+                ],
+                label: "D1",
+              },
+              {
+                data: [
+                  {
+                    x: 1,
+                    y: -2,
+                  },
+                  {
+                    x: 2,
+                    y: 7,
+                  },
+                  {
+                    x: 3,
+                    y: 9,
+                  },
+                  {
+                    x: 4,
+                    y: 3,
+                  },
+                ],
+                label: "D2",
+              },
+            ],
+          }}
+          options={{
+            scales: {
+              x: { type: "linear", min: 0, max: 5 },
+              y: { min: -4, max: 12 },
+            },
+            plugins: {
+              dragData: {
+                onDrag(event, di, index, value) {
+                  console.log("drag", { event, di, index, value });
+                },
+                onDragEnd(event, di, index, value) {
+                  console.log("drag end", { event, di, index, value });
+                },
+                onDragStart(event, di, index, value) {
+                  console.log("drag start", { event, di, index, value });
+                },
+              },
+            },
+          }}
+        />
       </header>
     </div>
   );
